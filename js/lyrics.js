0f6c5337003f7da84a8e370812e04ddc926b6e17
Mohamed-Amine Benali <benali.medamine2002@gmail.com>
2024-08-14 20:49:29 +0100

    Initial commit

---
+++ js/lyrics.js
@@ -0,0 +1,116 @@
+const lyrics = [
+    {
+        text: "Imane’s 21, it’s a brand new day,",
+        time: 9,
+    },
+    {
+        text: "Let’s celebrate her in every way!",
+        time: 13
+    },
+    {
+        text: "She’s got a big heart, so full of love,",
+        time: 22.8
+    },
+    {
+        text: "Always helping others, like a gift from above.",
+        time: 26.8
+    },
+    {
+        text: "Loves her family, especially her mom,",
+        time: 30.9
+    },
+    {
+        text: "With Imane around, everything’s calm.",
+        time: 34.7
+    },
+    {
+        text: "21 now, she’s shining so bright,",
+        time: 39
+    },
+    {
+        text: "Imane’s the star, lighting up the night.",
+        time: 42.8
+    },
+    {
+        text: "Brave and strong, she knows what’s true,",
+        time: 46.8
+    },
+    {
+        text: "Today’s all about you, Imane, we love you!",
+        time: 50.2
+    },
+    {
+        text: "[Music]",
+        time: 55
+    },
+    {
+        text: "She’s great at explaining, makes it all clear,",
+        time: 61
+    },
+    {
+        text: "Remembers everything, she’s got no fear.",
+        time: 64.9
+    },
+    {
+        text: "Loves her series, but she likes to go,",
+        time: 69
+    },
+    {
+        text: "In a quiet room, she’ll make it flow.",
+        time: 73.2
+    },
+    {
+        text: "21 now, she’s shining so bright,",
+        time: 79.7
+    },
+    {
+        text: "Imane’s the star, lighting up the night.",
+        time: 83.7
+    },
+    {
+        text: "Brave and strong, she knows what’s true,",
+        time: 87.8
+    },
+    {
+        text: "Today’s all about you, Imane, we love you!",
+        time: 90.8
+    },
+    {
+        text: "She’s a bit scared of the dentist’s chair,",
+        time: 95.2
+    },
+    {
+        text: "But when it’s time to sleep, she’s without a care.",
+        time: 98.8
+    },
+    {
+        text: "Energetic, fun, always on the go,",
+        time: 102.8
+    },
+    {
+        text: "Imane, you’re the one we all know!",
+        time: 106.85
+    },
+    {
+        text: "Happy 21st, let’s make some noise,",
+        time: 113
+    },
+    {
+        text: "Imane, you bring us all so much joy.",
+        time: 116
+    },
+    {
+        text: "This day is yours, so let’s sing it loud,",
+        time: 119.9
+    },
+    {
+        text: "Imane, you make us all so proud!",
+        time: 124
+    },
+    {
+        text: "[END]",
+        time: 131
+    },
+];
+
+export default lyrics;
---
+++ js/script.js
@@ -0,0 +1,157 @@
+import WaveSurfer from './wavesurfer.esm.js';
+import lyrics from './lyrics.js';
+
+
+const wavesurfer = WaveSurfer.create({
+    container: '#waves-container',
+    waveColor: '#020202',
+    progressColor: '#89c9ff',
+    url: 'assets/audios/Imane\'s pop song.mp3',
+});
+
+const CLASSES = {
+    buttonPlaying: "gift-card__audio-container__play-button--playing",
+    buttonPaused: "gift-card__audio-container__play-button--paused",
+}
+
+const giftCard = document.querySelector("[data-gift-card]");
+const audioBackground = document.querySelector("[data-audio-background]");
+const playButton = document.querySelector("[data-play-audio-btn]");
+const boxBody = document.querySelector("[data-box-body]");
+const currentLyric = document.querySelector("[data-current-lyric]");
+const fullLyricsButton = document.querySelector("[data-full-lyrics-btn]");
+const playlistButton = document.querySelector("[data-playlist-btn]");
+
+const clickTheGiftElement = document.querySelector("[data-click-the-gift]");
+
+const fullLyricsDialog = document.querySelector("[data-full-lyrics-dialog]");
+const playlistDialog = document.querySelector("[data-playlist-dialog]");
+const dialogCloseCrosses = document.querySelectorAll("[data-dialog-close-cross]")
+
+
+
+window.addEventListener('click', e => {
+    const audioEffectsArr = ["happy-birthday-to-you-made-by-me.mp3", "happy-birthday-voiced.mp3", "happy-birthday.mp3"];
+    let i = 0;
+    audioBackground.play();
+    // playAllAudios();
+
+    audioBackground.addEventListener("ended", e => {
+        if (i === 0) {
+            boxBody.addEventListener('click', e => {
+                clickTheGiftElement.remove();
+                changeBackgroundAudioSrc('assets/audios/effects/trumpet.m4a');
+                audioBackground.play();
+                i = Infinity;
+                Draw();
+                e.currentTarget.classList.add('opened');
+                setTimeout(() => {
+                    boxBody.parentElement.remove();
+                }, 5000);
+                giftCard.classList.remove("d-none");
+            }, { once: true });
+        }
+
+        if (i === Infinity) return
+        if (i >= audioEffectsArr.length) {
+            clickTheGiftElement.classList.remove('d-none');
+            return;
+        }
+        changeBackgroundAudioSrc(`assets/audios/effects/${audioEffectsArr[i]}`);
+        audioBackground.play();
+        i++;
+    })
+}, { once: true })
+
+async function playAllAudios() {
+    const audios = Array.from(document.querySelectorAll("[data-audio-background]")).filter(p => p !== audioBackground);
+    for (let audio of audios) {
+        await wait(600);
+        audio.play();
+    }
+}
+
+playButton?.addEventListener('click', e => {
+    if (wavesurfer.isPlaying()) {
+        wavesurfer.pause();
+        playButton.classList.add(CLASSES.buttonPaused);
+        playButton.classList.remove(CLASSES.buttonPlaying);
+    }
+    else {
+        wavesurfer.play();
+        playButton.classList.add(CLASSES.buttonPlaying);
+        playButton.classList.remove(CLASSES.buttonPaused);
+    }
+
+});
+
+
+
+wavesurfer.on('timeupdate', time => {
+    const lyricIndex = syncLyrics(time);
+    if (lyricIndex == null) return currentLyric.textContent = "";
+    currentLyric.textContent = lyrics[lyricIndex].text;
+});
+
+function showCard() {
+
+}
+
+function wait(ms) {
+    return new Promise(res => {
+        setTimeout(() => res(), ms);
+    })
+}
+
+function syncLyrics(time) {
+    const times = [];
+    for (let lyric of lyrics) {
+        const score = time - lyric.time;
+        if (score >= 0)
+            times.push(score);
+
+    }
+    const closest = Math.min(...times);
+    if (times.length === 0) return;
+    return times.indexOf(closest);
+}
+
+function changeBackgroundAudioSrc(src) {
+    audioBackground.firstElementChild.src = src;
+    audioBackground.currentTime = 0;
+    audioBackground.load();
+}
+
+
+fullLyricsButton.addEventListener('click', e => {
+    e.preventDefault();
+    fullLyricsDialog.showModal();
+    stopAllAudios();
+});
+fullLyricsDialog.onclick = e => {
+    if (e.target === fullLyricsDialog) fullLyricsDialog.close();
+}
+
+playlistDialog.onclick = e => {
+    if (e.target === playlistDialog) playlistDialog.close();
+}
+
+playlistButton.addEventListener('click', e => {
+    e.preventDefault();
+    playlistDialog.showModal();
+    stopAllAudios();
+});
+
+dialogCloseCrosses.forEach(ele => {
+    ele.onclick = e => {
+        e.currentTarget.parentElement.close();
+        stopAllAudios();
+    }
+});
+
+function stopAllAudios() {
+    document.querySelectorAll('audio').forEach(el => el.pause());
+    wavesurfer.pause();
+    playButton.classList.add(CLASSES.buttonPaused);
+    playButton.classList.remove(CLASSES.buttonPlaying);
+}
